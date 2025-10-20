32b699b481512c684371b68d9c988f9d1562afbc
Lawrence <Electricalwolf5@gmail.com>
2025-10-20 12:53:57 -0400

    Added Source File
    C# Development attempts have started back up. Current C# code writes a message to log stating the mod has loaded itself.

---
+++ Source/Mechadendrites_Expanded/Mechadendrites/Loading.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Marak.Mechadendrites.Bootstrap
+{
+    [StaticConstructorOnStartup]
+    public static class MechadendritesExpandedLoader
+    {
+        static MechadendritesExpandedLoader()
+        {
+            Log.Message("[Mechadendrites Expanded] Loaded");
+        }
+    }
+}
