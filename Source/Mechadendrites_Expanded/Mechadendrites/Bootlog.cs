2f3e6e2363d6c0038d34a58cec04dc243796a0c6
Lawrence <Electricalwolf5@gmail.com>
2025-10-24 17:38:22 -0400

    Foundations of future Mod settings
    Setting up the foundations for future mod settings down the line. Currently, it's only examples taken from the wiki. Future goal: Allow how many mechadendrite slots people want varying from 4-8 for simplicity. Base amount: 6. May reduce to 4 if settings work out well.

---
+++ Source/Mechadendrites_Expanded/Mechadendrites/Bootlog.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+using MechadendritesExpanded;
+
+namespace MechadendritesExpanded
+{
+    [StaticConstructorOnStartup]
+    public static class MechadendritesExpandedLog
+    {
+        static MechadendritesExpandedLog()
+        {
+            Log.Message("[Mechadendrites Expanded] Revision: Zagreus Kane Loaded");
+        }
+    }
+}
