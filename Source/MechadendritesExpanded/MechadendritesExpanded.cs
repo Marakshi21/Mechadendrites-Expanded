421b7a9aac55a3d6954a0c1c3687f5f6f7d9009e
Lawrence Whitby <electricalwolf5@gmail.com>
2025-12-18 13:49:15 -0500

    I hate Mod Settings.
    Fuck Rider. Trying to use this shit to set things up properly and develop shit is so damn annoying. Mod settings are, for some reason, absolutely fucking insane and refuses to work properly. And I have no idea why.

--- Source/MechadendritesExpanded/MechadendritesExpanded.cs
+++ Source/MechadendritesExpanded/MechadendritesExpanded.cs
@@ -1,12 +1,15 @@
 using System.Reflection;
 using HarmonyLib;
 using Verse;
+
 namespace MechadendritesExpanded
+
 {
     public class MechadendritesExpanded : Mod
     {
-        public MechadendritesExpanded(ModContentPack content) : base(content)
-        { 
+        public MechadendritesExpanded(ModContentPack content)
+            : base(content)
+        {
             new Harmony("com.mechadendritesexpanded").PatchAll(Assembly.GetExecutingAssembly());
         }
     }
--- Source/MechadendritesExpanded/ModSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace MechadendritesExpanded
-{
-    public class ModSettings
-    {
-        
-    }
-}
