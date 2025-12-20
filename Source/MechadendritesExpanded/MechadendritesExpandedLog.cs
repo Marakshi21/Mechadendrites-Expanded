b6191e1d996595198374c7df2a70dec116a76a19
Lawrence Whitby <electricalwolf5@gmail.com>
2025-12-20 13:56:37 -0500

    DLL removal
    Get these DLL files out of my git repo

--- Source/MechadendritesExpanded/MechadendritesExpandedLog.cs
+++ Source/MechadendritesExpanded/MechadendritesExpandedLog.cs
@@ -2,10 +2,11 @@ using Verse;
 
 namespace MechadendritesExpanded
 {
+    // Print out a log to confirm the mod loaded properly
     [StaticConstructorOnStartup]
-    public static class MechadendritesExpandedLogging
+    public static class MechadendritesExpandedLog
     {
-        static MechadendritesExpandedLogging()
+        static MechadendritesExpandedLog()
         {
             Log.Message("[Mechadendrites Expanded] Revision: Nova loaded");
             Log.Warning("You are running the Nova Branch of Mechadendrites Expanded. This branch is experimental and unstable. Proceed with caution."); // Remove for main release
