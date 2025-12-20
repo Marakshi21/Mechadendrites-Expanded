f97667bf52e3507d837973a596e6e062394b5d85
Lawrence Whitby <electricalwolf5@gmail.com>
2025-12-20 16:31:38 -0500

    Added New Template
    Added a new refined template file to use for designing new mechadendrites and make additions faster. Hopefully not plagued by as many issues as the old one was

--- Source/MechadendritesExpanded/MechadendritesExpandedLog.cs
+++ Source/MechadendritesExpanded/MechadendritesExpandedLog.cs
@@ -9,7 +9,7 @@ namespace MechadendritesExpanded
         static MechadendritesExpandedLog()
         {
             Log.Message("[Mechadendrites Expanded] Revision: Nova loaded");
-            Log.Warning("You are running the Nova Branch of Mechadendrites Expanded. This branch is experimental and unstable. Proceed with caution."); // Remove for main release
+            Log.Error("You are running the Nova Branch of Mechadendrites Expanded. This branch is experimental and unstable. Proceed with caution."); // Remove for main release
         }
     }
 }
