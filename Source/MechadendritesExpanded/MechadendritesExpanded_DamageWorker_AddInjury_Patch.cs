ab6bd329f62c7cd01af838d1078c6acf8aadc1df
Lawrence Whitby <electricalwolf5@gmail.com>
2025-12-21 16:24:38 -0500

    Damage Worker Patch Difficulties
    Trying to figure out how to not apply damage to a mechadendrite slot that is lacking an added body part is difficult. Send help.

--- Source/MechadendritesExpanded/MechadendritesExpanded_DamageWorker_AddInjury_Patch.cs
+++ Source/MechadendritesExpanded/MechadendritesExpanded_DamageWorker_AddInjury_Patch.cs
@@ -1,15 +1,23 @@
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using HarmonyLib;
 using Verse;
-
+/*
+ The following Patch is meant to intercept the DamageWorker and check to see if it's targeting
+ a mechadendrite slot that lacks an added body part of some kind
+ and if so, do not apply damage onto it
+*/
 namespace MechadendritesExpanded
 {
-    /*
+    /* I think I may be attaching onto the wrong method for what I want to do
     [HarmonyPatch(typeof (DamageWorker.DamageResult), "ApplyDamageToPart")]
     public static class MechadendritesExpanded_DamageWorker_AddInjury_Patch
     {
-        [HarmonyPrefix]
         private static void Prefix(DamageWorker.DamageResult __result)
-        BodyPartRecord partFromDamageInfo = 
+
+        private BodyPartRecord partFromDamageInfo = this.GetExactPartFromDamageInfo(dinfo, pawn);
     }
     */
 }
